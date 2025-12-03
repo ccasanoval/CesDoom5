2dada94eab8df87d2978f2860c0cbd9adf63ae99
Cesar Casanova <cesarcl@ext.inditex.com>
2025-12-03 17:06:24 +0100

    Initial commit

---
+++ game.gd
@@ -0,0 +1,25 @@
+extends Node3D
+
+#RESOURCES:
+#----------
+
+#3D
+#https://www.fab.com/listings/1353e155-41de-4f7f-a6a3-a4d6f72b115a
+
+#Textures
+#https://ambientcg.com/list?category=NightSkyHDRISubstance&type=substance&sort=popular
+
+#Plugins
+#https://github.com/Saulo-de-Souza/VirtualJoystick
+
+#Tutorial
+#https://www.gdquest.com/library/first_3d_game_godot4_arena_fps/#creating-the-level
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
---
+++ player/player.gd
@@ -0,0 +1,49 @@
+extends CharacterBody3D
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+@onready var joyTrans = $JoystickTrans
+@onready var joyRot = $JoystickRot
+
+
+func _physics_process(delta: float) -> void:
+	
+	#### GRAVITY
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	#### JUMP
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	#### KEYBOARD
+	# Get the input direction and handle the movement/deceleration.
+	#var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+	var direction := (transform.basis * Vector3(input.x, 0, input.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+	
+	#### JOYSTICKS	
+	input = joyRot.get_value()
+	if input:
+		#%Camera3D.rotation_degrees.y -= input.x * SPEED/2
+		rotation_degrees.y -= input.x * SPEED / 2
+		%Camera3D.rotation_degrees.x -= input.y * SPEED/2
+		%Camera3D.rotation_degrees.x = clamp(%Camera3D.rotation_degrees.x, -40, +40)
+
+	input = joyTrans.get_value()
+	if input:
+		direction = (transform.basis * Vector3(input.x, 0, input.y)).normalized()
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+		
+	move_and_slide()
+
+	#if Input.is_action_pressed("shoot") and %Timer.is_stopped():
+	#	shoot_bullet()
