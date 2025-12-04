90f46b225cd3432eb583d8692c0bc9b3eab8d1f5
Cesar Casanova <cesarcl@ext.inditex.com>
2025-12-04 18:03:41 +0100

    Working with mob animation and movement

--- mobs/mob_fishie.gd
+++ mobs/mob_fishie.gd
@@ -3,12 +3,16 @@ extends CharacterBody3D
 var player = null
 @export var player_path: NodePath
 @onready var nav_agent = %NavigationAgent3D
+@onready var anim_tree = %AnimationTree
+var anim_state_machine
 
-const SPEED = 4.0
-const JUMP_VELOCITY = 4.5
+const SPEED = 2.0
+const RANGE_PLAYER_AT_SIGHT = 10
+const RANGE_PLAYER_IS_NEXT = 3
 
 func _ready() -> void:
 	player = get_node(player_path)
+	anim_state_machine = anim_tree.get("parameters/playback")
 	
 func _process(delta: float) -> void:
 	velocity = Vector3.ZERO
@@ -17,9 +21,26 @@ func _process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	
-	#### SEARCH AND DESTROY
-	nav_agent.set_target_position(player.global_transform.origin)
-	var next_nav_point = nav_agent.get_next_path_position()
-	velocity = (next_nav_point - global_transform.origin).normalized() * SPEED
+	### ANIMATIONS	
+	var next = global_position.distance_to(player.global_position) < RANGE_PLAYER_IS_NEXT
+	var sight = global_position.distance_to(player.global_position) < RANGE_PLAYER_AT_SIGHT
+	anim_tree.set("parameters/conditions/player_at_sight", sight)
+	anim_tree.set("parameters/conditions/player_is_next", next)
+	anim_tree.set("parameters/conditions/player_not_at_sight", !sight)
+
+	match anim_state_machine.get_current_node():
+		"Idle_g":
+			#### DO NOTHING
+			pass
+		"Run_Aim_g":
+			#### SEARCH AND DESTROY
+			nav_agent.set_target_position(player.global_position)
+			var next_nav_point = nav_agent.get_next_path_position()
+			velocity = (next_nav_point - global_position).normalized() * SPEED
+			look_at(next_nav_point, Vector3.UP)
+			#TODO: Shoot
+		"Throw_g":
+			#### HIT
+			look_at(player.global_position, Vector3.UP)
 	
 	move_and_slide()
